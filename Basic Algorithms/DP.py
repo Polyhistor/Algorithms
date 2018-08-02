aa36d48988f56c5370752269c8a6c6ce249564da
polyhistor <pouya.ataei.7@gmail.com>
2018-08-02 21:19:29 +0430

    Second Commit

---
+++ Basic Algorithms/DP.py
@@ -0,0 +1,33 @@
+# Finding the number in middle of an array (this will serve as a part of a greater alogrithm, so you may night find it useful alone)
+
+# Integer: Dividingpoint (Integer: array[])
+#    Integer: number1 = array[0]
+#    Integer: number2 = array[<last index of array>]
+#    Integer: number2 = array[<last index of array> / 2]
+
+#    If (<number1 is between number2 and number3>) return number 1
+#    If (<number2 is between number1 and number3>) return number 2
+#    Return number3
+
+# End Middlevalue
+
+
+
+def DP (arr):
+
+    num1 = arr[0]
+    num2 = arr[-1]
+    num3 = arr[-1]/2
+
+    if (num2 <= num1 <= num3):
+        if (num1 <= num2 <= num3):
+            return num3
+        else:
+            return num2
+    return num1
+
+
+
+arr = [1,2,3,4,5]
+ans = DP(arr)
+print(ans)
